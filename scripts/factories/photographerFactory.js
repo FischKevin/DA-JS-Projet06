e5dce90a1c6a17bb4457078fd8d558da292e1f58
Kévin Fischer <kevinfischer@live.fr>
2023-07-07 17:32:07 +0200

    eslint + fix on fetch data + begin index css

---
+++ scripts/factories/photographerFactory.js
@@ -0,0 +1,30 @@
+function photographerFactory(data) {
+  const {
+    name, portrait, city, country, tagline, price,
+  } = data;
+
+  const picture = `/assets/images/photographers/00-Portraits/${portrait}`;
+
+  function getUserCardDOM() {
+    const article = document.createElement('article');
+    const img = document.createElement('img');
+    img.setAttribute('src', picture);
+    const h2 = document.createElement('h2');
+    h2.textContent = name;
+    const h3 = document.createElement('h3');
+    h3.textContent = `${city}, ${country}`;
+    const h4 = document.createElement('h4');
+    h4.textContent = tagline;
+    const h5 = document.createElement('h5');
+    h5.textContent = price;
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(h4);
+    article.appendChild(h5);
+    return (article);
+  }
+  return {
+    name, picture, city, country, tagline, price, getUserCardDOM,
+  };
+}
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,24 +1,22 @@
 async function getPhotographers() {
-    return fetch('./data/photographers.json')
+  return fetch('./data/photographers.json')
     .then((response) => response.json())
     .then((data) => data.photographers);
 }
 
 async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
+  const photographersSection = document.querySelector('.photographer_section');
 
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
 }
 
 async function init() {
-    const photographers = await getPhotographers();
-    // const { photographers } = await getPhotographers();
-    displayData(photographers);
+  const photographers = await getPhotographers();
+  displayData(photographers);
 }
 
 init();
-    
