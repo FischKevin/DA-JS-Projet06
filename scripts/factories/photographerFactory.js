51c60d084b15ede48fb8409f64b3350cef4df9fc
Kévin Fischer <kevinfischer@live.fr>
2023-07-10 11:37:44 +0200

    added a wrapper for img

--- scripts/factories/photographerFactory.js
+++ scripts/factories/photographerFactory.js
@@ -7,8 +7,11 @@ function photographerFactory(data) {
 
   function getUserCardDOM() {
     const article = document.createElement('article');
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
     const img = document.createElement('img');
     img.setAttribute('src', picture);
+    wrapper.appendChild(img);
     const h2 = document.createElement('h2');
     h2.textContent = name;
     const h3 = document.createElement('h3');
@@ -17,7 +20,7 @@ function photographerFactory(data) {
     h4.textContent = tagline;
     const h5 = document.createElement('h5');
     h5.textContent = `${price}€/jour`;
-    article.appendChild(img);
+    article.appendChild(wrapper);
     article.appendChild(h2);
     article.appendChild(h3);
     article.appendChild(h4);
