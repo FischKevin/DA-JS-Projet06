0cea0e2777c7a9eac11128ee1f29927e98099797
Kévin Fischer <kevinfischer@live.fr>
2023-08-16 15:28:47 +0200

    :lipstick: added cross to close lightbox + display fix

--- scripts/utils/lightBox.js
+++ scripts/utils/lightBox.js
@@ -1,26 +1,33 @@
 export function openLightbox(picture, title) {
-  const lightBoxModal = document.createElement("div");
-  lightBoxModal.setAttribute("role", "modal");
-  lightBoxModal.className = "lightBox_Modal";
-  const lightBoxContent = document.createElement("div");
-  lightBoxContent.className = "lightBoxContent";
-  const lightBoxPrev = document.createElement("div");
-  lightBoxPrev.className = "lightBoxPrev";
-  lightBoxPrev.innerHTML = "<";
-  const lightBoxPicture = document.createElement("figure");
-  lightBoxPicture.className = "lightBoxPicture";
-  const lightBoxPictureSource = document.createElement("img");
-  lightBoxPictureSource.setAttribute("src", picture);
-  lightBoxPictureSource.setAttribute("alt", title);
-  lightBoxPictureSource.className = "lightBoxPictureSource";
-  const lightBoxPictureCaption = document.createElement("figcaption");
+  const lightBoxModal = document.createElement('div');
+  lightBoxModal.setAttribute('role', 'modal');
+  lightBoxModal.className = 'lightBox_Modal';
+  const lightBoxModalClose = document.createElement('i');
+  lightBoxModalClose.className = 'lightBoxModalClose';
+  lightBoxModalClose.classList.add('fa-solid');
+  lightBoxModalClose.classList.add('fa-close');
+  const lightBoxContent = document.createElement('div');
+  lightBoxContent.className = 'lightBoxContent';
+  const lightBoxPrev = document.createElement('i');
+  lightBoxPrev.className = 'lightBoxPrev';
+  lightBoxPrev.classList.add('fa-solid');
+  lightBoxPrev.classList.add('fa-chevron-left');
+  const lightBoxPicture = document.createElement('figure');
+  lightBoxPicture.className = 'lightBoxPicture';
+  const lightBoxPictureSource = document.createElement('img');
+  lightBoxPictureSource.setAttribute('src', picture);
+  lightBoxPictureSource.setAttribute('alt', title);
+  lightBoxPictureSource.className = 'lightBoxPictureSource';
+  const lightBoxPictureCaption = document.createElement('figcaption');
   lightBoxPictureCaption.innerHTML = title;
-  lightBoxPictureCaption.className = "lightBoxPictureCaption";
-  const lightBoxNext = document.createElement("div");
-  lightBoxNext.className = "lightBoxNext";
-  lightBoxNext.innerHTML = ">";
+  lightBoxPictureCaption.className = 'lightBoxPictureCaption';
+  const lightBoxNext = document.createElement('i');
+  lightBoxNext.className = 'lightBoxNext';
+  lightBoxNext.classList.add('fa-solid');
+  lightBoxNext.classList.add('fa-chevron-right');
   document.body.appendChild(lightBoxModal);
   lightBoxModal.appendChild(lightBoxContent);
+  lightBoxContent.appendChild(lightBoxModalClose);
   lightBoxContent.appendChild(lightBoxPrev);
   lightBoxContent.appendChild(lightBoxPicture);
   lightBoxContent.appendChild(lightBoxNext);
@@ -28,8 +35,8 @@ export function openLightbox(picture, title) {
   lightBoxPicture.appendChild(lightBoxPictureCaption);
 }
 
-const mediaCollection = document.getElementsByClassName("photographerMedia");
+const mediaCollection = document.getElementsByClassName('photographerMedia');
 
 for (const mediaElement of mediaCollection) {
-  mediaElement.addEventListener("click", openLightbox);
+  mediaElement.addEventListener('click', openLightbox);
 }
