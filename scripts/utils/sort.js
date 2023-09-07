f5a7f8703ca5168d7bcb70ad6f3424b6824878f3
Kévin Fischer <kevinfischer@live.fr>
2023-09-07 13:59:00 +0200

    :bug: fixed pixelated index img on Chrome

--- scripts/utils/sort.js
+++ scripts/utils/sort.js
@@ -5,14 +5,17 @@ import { displayMediaData } from '../pages/photographer.js';
 function displaySortMenu() {
   const sortBlockOptions = document.querySelector('.options');
   const selectedOption = document.querySelector('.selected-option');
+
   if (sortBlockOptions.style.display === 'block') {
     sortBlockOptions.style.display = 'none';
     sortBlockOptions.classList.remove('open');
     selectedOption.classList.remove('open');
+    selectedOption.setAttribute('aria-expanded', 'false');
   } else {
     sortBlockOptions.style.display = 'block';
     sortBlockOptions.classList.add('open');
     selectedOption.classList.add('open');
+    selectedOption.setAttribute('aria-expanded', 'true');
   }
 }
 
@@ -24,6 +27,11 @@ function handleOptionClick(event) {
   const tempText = selectedOption.textContent;
   selectedOption.textContent = clickedOption.textContent;
   clickedOption.textContent = tempText;
+  // Get the sorting criteria from the clicked option's data-value
+  const sortingCriteria = clickedOption.dataset.value;
+
+  sortAndDisplayMedia(sortingCriteria);
+
   displaySortMenu();
 }
 // each time an option is selected, handleOptionClick managed sort menu
@@ -96,7 +104,7 @@ document.querySelector('.custom-select').addEventListener('click', (event) => {
   }
 });
 
-// sort menu : function to sort and display media
+// function to sort and display media
 function sortAndDisplayMedia(criteria) {
   if (criteria === 'popularity') {
     globalState.media.sort((a, b) => b.likes - a.likes);
@@ -107,3 +115,72 @@ function sortAndDisplayMedia(criteria) {
   }
   displayMediaData(globalState.media);
 }
+
+// display sort menu on keyboard event
+document
+  .querySelector('.selected-option')
+  .addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      displaySortMenu();
+      event.preventDefault();
+    }
+  });
+
+document.querySelectorAll('.option').forEach((optionElement) => {
+  optionElement.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      handleOptionClick(event);
+      displaySortMenu(); // Fermez le menu après avoir sélectionné une option
+      event.preventDefault();
+    }
+  });
+});
+
+// // handle option selection on keyboard event
+// document
+//   .querySelector('.custom-select')
+//   .addEventListener('keydown', (event) => {
+//     if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+//       let current = Array.from(sortOptions).findIndex(
+//         (option) => option === event.target
+//       );
+//       if (event.key === 'ArrowDown' && current < sortOptions.length - 1) {
+//         sortOptions[current + 1].focus();
+//       } else if (event.key === 'ArrowUp' && current > 0) {
+//         sortOptions[current - 1].focus();
+//       }
+//       event.preventDefault();
+//     }
+//   });
+
+document.querySelectorAll('.option').forEach((optionElement) => {
+  optionElement.addEventListener('keydown', (event) => {
+    let sortOptionsArray = Array.from(sortOptions);
+    let currentIndex = sortOptionsArray.findIndex(
+      (option) => option === event.target
+    );
+
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      console.log('ArrowDown pressed');
+      if (currentIndex < sortOptionsArray.length - 1) {
+        sortOptionsArray[currentIndex + 1].focus();
+      }
+    } else if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      console.log('ArrowUp pressed');
+      if (currentIndex > 0) {
+        sortOptionsArray[currentIndex - 1].focus();
+      }
+    }
+  });
+});
+
+// close sort menu on escape key
+document
+  .querySelector('.custom-select')
+  .addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+      displaySortMenu();
+    }
+  });
